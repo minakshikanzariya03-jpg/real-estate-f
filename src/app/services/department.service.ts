a8fc9dd6490ab5e8e4528e1e960a2240be5e24d6
minakshikanzariya <minakshi@kanzariya.com>
2026-06-27 14:53:52 +0530

    auth page

--- src/app/services/department.service.ts
+++ src/app/services/department.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
- apiUrl = 'http://localhost:3000';
+apiUrl = 'http://192.168.0.34:3000';
+//  apiUrl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
 
